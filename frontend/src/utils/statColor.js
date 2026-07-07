5e71b7775421ea8ec692f4548aa433f6a1ea7f29
Steven Fitzgerald <stevenfitzg@gmail.com>
2026-07-07 01:56:32 -0400

    adjusted statbar colors to more accurately match game

--- frontend/src/utils/statColor.js
+++ frontend/src/utils/statColor.js
@@ -1,6 +1,7 @@
 export function getStatColor(value) {
-  if (value < 40) return '#E24B4A';
-  if (value < 60) return '#D4537E';
-  if (value < 80) return '#0C447C';
+  if (value < 55) return '#E24B4A';
+  if (value < 70) return '#D4537E';
+  if (value < 80) return '#5e0c70';
+  if (value < 91) return '#0C447C';
   return '#378ADD';
 }
