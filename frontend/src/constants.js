4a4865a84b399ee51f27a6a6a96cfadf170e5193
Steven Fitzgerald <stevenfitzg@gmail.com>
2026-07-09 16:30:15 -0400

    Update site notice message

--- frontend/src/constants.js
+++ frontend/src/constants.js
@@ -65,4 +65,4 @@ export const POSITION_SORT_ORDER = [
   "C", "1B", "2B", "3B", "SS", "IF", "LF", "CF", "RF", "OF"
 ];
 
-export const SITE_NOTICE = "In case of \"Failed to load ____\", refresh page | Stadium photos not yet uploaded - Working on it | Contact developer stevenfitzg@gmail.com"
+export const SITE_NOTICE = "In case of \"Failed to load ____\", refresh page | Stadium photos not yet uploaded - Working on it"
