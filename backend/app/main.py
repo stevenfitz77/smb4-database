3c327e079d21680787c3c1767a558ac69c3eaa6e
Steven Fitzgerald <stevenfitzg@gmail.com>
2026-07-09 16:24:51 -0400

    Fix missing bug_reports import in main.py

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,7 +1,7 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 
-from .routers import teams, players
+from .routers import teams, players, bug_reports
 
 app = FastAPI()
 
