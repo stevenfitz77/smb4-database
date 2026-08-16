768868dc54712ea6d9e4f9822fd4e763ba33e004
Steven Fitzgerald <stevenfitzg@gmail.com>
2026-08-16 02:02:00 -0400

    Added text shadow outline to stat bars on player detail page, modified statColor.js to better reflect in-game colors

--- frontend/src/utils/statColor.js
+++ frontend/src/utils/statColor.js
@@ -1,7 +1,14 @@
 export function getStatColor(value) {
-  if (value < 55) return '#E24B4A';
-  if (value < 70) return '#D4537E';
-  if (value < 80) return '#5e0c70';
-  if (value < 91) return '#0C447C';
+  if (value < 10) return '#a60012';
+  if (value < 20) return '#b80414';
+  if (value < 30) return '#a10011';
+  if (value < 40) return '#a30011';
+  if (value < 50) return '#9c0011';
+  if (value < 58) return '#93002f';
+  if (value < 70) return '#7c006b';
+  if (value < 75) return '#58009e';
+  if (value < 80) return '#4f00a6';
+  if (value < 90) return '#2d00b0';
+  if (value < 95) return '#003fb5';
   return '#378ADD';
 }
